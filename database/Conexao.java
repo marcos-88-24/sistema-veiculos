a79d32d8db5f5826bb9206d9ba2a8cd056816876
MARCOS DOS SANTOS <mrcssantos174@gmail.com>
2025-11-15 16:44:12 -0300

    Update Conexao.java

--- database/Conexao.java
+++ database/Conexao.java
@@ -1,1 +1,21 @@
 
+package br.com.fecaf.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/sistema_veiculos";
+    private static final String USER = "root";
+    private static final String PASS = "8627";
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+            return null;
+        }
+    }
+}
