0ac29b88b93da7ff75d3ca5501d035cbe34f60b1
MARCOS DOS SANTOS <mrcssantos174@gmail.com>
2025-11-15 16:45:35 -0300

    Add MarcaModelo class with properties and methods

--- model/MarcaModelo.java
+++ model/MarcaModelo.java
@@ -1,1 +1,31 @@
+package br.com.fecaf.model;
+
+public class MarcaModelo {
+
+    private int id;
+    private String marca;
+    private String modelo;
+
+    public MarcaModelo() {}
+
+    public MarcaModelo(int id, String marca, String modelo) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getMarca() { return marca; }
+    public void setMarca(String marca) { this.marca = marca; }
+
+    public String getModelo() { return modelo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+
+    @Override
+    public String toString() {
+        return marca + " - " + modelo;
+    }
+}
 
